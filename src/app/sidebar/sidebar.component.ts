fae2dea85a2cb2f79dc2a817f700df04c152e05d
Neo <neo.javadev@gmail.com>
2023-01-01 21:15:30 +0100

    navbar and footer

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -9,14 +9,13 @@ export interface RouteInfo {
 }
 
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard',     title: 'Dashboard',         icon:'nc-bank',       class: '' },
-    { path: '/icons',         title: 'Icons',             icon:'nc-diamond',    class: '' },
-    { path: '/maps',          title: 'Maps',              icon:'nc-pin-3',      class: '' },
-    { path: '/notifications', title: 'Notifications',     icon:'nc-bell-55',    class: '' },
     { path: '/user',          title: 'User Profile',      icon:'nc-single-02',  class: '' },
-    { path: '/table',         title: 'Table List',        icon:'nc-tile-56',    class: '' },
-    { path: '/typography',    title: 'Typography',        icon:'nc-caps-small', class: '' },
-    { path: '/upgrade',       title: 'Upgrade to PRO',    icon:'nc-spaceship',  class: 'active-pro' },
+    { path: '/dashboard',     title: 'Dashboard',         icon:'nc-bank',       class: '' },
+    { path: '/user',          title: 'Users',      icon:'nc-single-02',  class: '' },
+    { path: '/maps',          title: 'Projects',              icon:'nc-pin-3',      class: '' },
+    { path: '/notifications', title: 'Tasks',     icon:'nc-bell-55',    class: '' },
+    { path: '/notifications', title: '!NOTIFICATIONS!',     icon:'nc-bell-55',    class: '' },
+    { path: '/table',         title: 'Table List',        icon:'nc-tile-56',    class: '' }
 ];
 
 @Component({
