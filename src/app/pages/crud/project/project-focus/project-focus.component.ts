353a0b22a91f41f7c6beff44ad2b8760ef780c32
Neo <neo.javadev@gmail.com>
2023-01-04 13:57:15 +0100

    Charts Done

--- src/app/pages/crud/project/project-focus/project-focus.component.ts
+++ src/app/pages/crud/project/project-focus/project-focus.component.ts
@@ -20,6 +20,8 @@ export class ProjectFocusComponent implements OnInit {
   public canvas : any;
   public ctx;
   public chartEmail;
+  public chartOverheads;
+  public chartHr;
 
   constructor(private adminService: AdminService,private route: ActivatedRoute) { }
 
@@ -48,9 +50,10 @@ export class ProjectFocusComponent implements OnInit {
       let cost = res.cost;
       let revenue = res.revenue;
 
-      this.canvas = document.getElementById("chartEmail");
+      // CHART OVERHEADS
+      this.canvas = document.getElementById("chartOverheads");
       this.ctx = this.canvas.getContext("2d");
-      this.chartEmail = new Chart(this.ctx, {
+      this.chartOverheads = new Chart(this.ctx, {
         type: 'pie',
         data: {
           labels: [1, 2, 3],
@@ -115,6 +118,138 @@ export class ProjectFocusComponent implements OnInit {
           },
         }
       });
+
+      //CHART HR
+      this.canvas = document.getElementById("chartHr");
+      this.ctx = this.canvas.getContext("2d");
+      this.chartOverheads = new Chart(this.ctx, {
+        type: 'pie',
+        data: {
+          labels: [1, 2, 3],
+          datasets: [{
+            label: "Cost Breakdown",
+            pointRadius: 0,
+            pointHoverRadius: 0,
+            backgroundColor: [
+              '#3e5170',
+              '#4acccd',
+
+            ],
+            borderWidth: 0,
+            data: [management, developer]
+          }]
+        },
+
+        options: {
+
+          legend: {
+            display: false
+          },
+
+          pieceLabel: {
+            render: 'percentage',
+            fontColor: ['white'],
+            precision: 2
+          },
+
+          tooltips: {
+            enabled: false
+          },
+
+          scales: {
+            yAxes: [{
+
+              ticks: {
+                display: false
+              },
+              gridLines: {
+                drawBorder: false,
+                zeroLineColor: "transparent",
+                color: 'rgba(255,255,255,0.05)'
+              }
+
+            }],
+
+            xAxes: [{
+              barPercentage: 1.6,
+              gridLines: {
+                drawBorder: false,
+                color: 'rgba(255,255,255,0.1)',
+                zeroLineColor: "transparent"
+              },
+              ticks: {
+                display: false,
+              }
+            }]
+          },
+        }
+      });
+
+      // CHART PROFITABILITY
+      this.canvas = document.getElementById("chartProfitability");
+      this.ctx = this.canvas.getContext("2d");
+      this.chartOverheads = new Chart(this.ctx, {
+        type: 'pie',
+        data: {
+          labels: [1, 2, 3],
+          datasets: [{
+            label: "Profitability",
+            pointRadius: 0,
+            pointHoverRadius: 0,
+            backgroundColor: [
+              '#3e5170',
+              '#4acccd'
+            ],
+            borderWidth: 0,
+            data: [revenue, cost]
+          }]
+        },
+
+        options: {
+
+          legend: {
+            display: false
+          },
+
+          pieceLabel: {
+            render: 'percentage',
+            fontColor: ['white'],
+            precision: 2
+          },
+
+          tooltips: {
+            enabled: false
+          },
+
+          scales: {
+            yAxes: [{
+
+              ticks: {
+                display: false
+              },
+              gridLines: {
+                drawBorder: false,
+                zeroLineColor: "transparent",
+                color: 'rgba(255,255,255,0.05)'
+              }
+
+            }],
+
+            xAxes: [{
+              barPercentage: 1.6,
+              gridLines: {
+                drawBorder: false,
+                color: 'rgba(255,255,255,0.1)',
+                zeroLineColor: "transparent"
+              },
+              ticks: {
+                display: false,
+              }
+            }]
+          },
+        }
+      });
+
     });
 
 
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -11,9 +11,9 @@ export interface RouteInfo {
 export const ROUTES: RouteInfo[] = [
     { path: '/user',           title: 'User Profile',      icon:'nc-single-02',  class: '' },
     { path: '/dashboard',      title: 'Dashboard',         icon:'nc-chart-pie-36',  class: '' },
-    { path: '/users',          title: 'User List',         icon:'nc-circle-10',  class: '' },
-    { path: '/projects',       title: 'Project List',      icon:'nc-chart-bar-32',       class: '' },
-    { path: '/tasks',          title: 'Task List',         icon:'nc-tile-56',  class: '' },
+    { path: '/users',          title: 'Users',         icon:'nc-circle-10',  class: '' },
+    { path: '/projects',       title: 'Projects',      icon:'nc-chart-bar-32',       class: '' },
+    { path: '/tasks',          title: 'Tasks',         icon:'nc-tile-56',  class: '' },
     { path: '/notifications',  title: 'Notification',      icon:'nc-bell-55',    class: '' }
 ];
 
