5e08adf225458b92a009895a9e0f8a49ca7b4670
Neo <neo.javadev@gmail.com>
2023-01-05 20:56:58 +0100

    Dashboard in progress

--- src/app/pages/crud/project/project-focus/project-focus.component.ts
+++ src/app/pages/crud/project/project-focus/project-focus.component.ts
@@ -1,42 +1,40 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { AdminService } from 'app/layouts/admin-layout/admin.service';
-import Chart from 'chart.js';
-import { Project } from '../model/project';
-
+import { Component, Input, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { AdminService } from "app/layouts/admin-layout/admin.service";
+import Chart from "chart.js";
+import { Project } from "../model/project";
 
 @Component({
-  selector: 'app-project-focus',
-  templateUrl: './project-focus.component.html',
-  styles: [
-  ]
+  selector: "app-project-focus",
+  templateUrl: "./project-focus.component.html",
+  styles: [],
 })
 export class ProjectFocusComponent implements OnInit {
+  project: Project | any;
+  projects: Project[] | any;
 
-
-  project: Project|any;
-  projects: Project[]|any;
-
-  public canvas : any;
+  public canvas: any;
   public ctx;
-  public chartEmail;
   public chartOverheads;
   public chartHr;
+  public chartProfitability;
 
-  constructor(private adminService: AdminService,private route: ActivatedRoute) { }
+  constructor(
+    private adminService: AdminService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-
     this.projects = this.adminService.getProjects();
-    const projectId: string|null = this.route.snapshot.paramMap.get('id');
+    const projectId: string | null = this.route.snapshot.paramMap.get("id");
 
-    if(projectId) {
-      this.adminService.getProjectById(+projectId)
-      .subscribe(project => this.project = project);
+    if (projectId) {
+      this.adminService
+        .getProjectById(+projectId)
+        .subscribe((project) => (this.project = project));
     }
 
-    this.adminService.getProjectById(+projectId)
-    .subscribe(res => {
+    this.adminService.getProjectById(+projectId).subscribe((res) => {
       let management = res.management;
       let developer = res.developer;
 
@@ -54,204 +52,210 @@ export class ProjectFocusComponent implements OnInit {
       this.canvas = document.getElementById("chartOverheads");
       this.ctx = this.canvas.getContext("2d");
       this.chartOverheads = new Chart(this.ctx, {
-        type: 'pie',
+        type: "pie",
         data: {
           labels: [1, 2, 3],
-          datasets: [{
-            label: "Cost Breakdown",
-            pointRadius: 0,
-            pointHoverRadius: 0,
-            backgroundColor: [
-              '#49c6ff',
-              '#174d8b',
-              '#c0e6f5',
-              '#952bc7',
-              '#8a76fd',
-              '#961919d0'
-            ],
-            borderWidth: 0,
-            data: [software, hardware, premises, furniture, sourcing, distribution]
-          }]
+          datasets: [
+            {
+              label: "Cost Breakdown",
+              pointRadius: 0,
+              pointHoverRadius: 0,
+              backgroundColor: [
+                "#49c6ff",
+                "#174d8b",
+                "#c0e6f5",
+                "#952bc7",
+                "#8a76fd",
+                "#961919d0",
+              ],
+              borderWidth: 0,
+              data: [
+                software,
+                hardware,
+                premises,
+                furniture,
+                sourcing,
+                distribution,
+              ],
+            },
+          ],
         },
 
         options: {
-
           legend: {
-            display: false
+            display: false,
           },
 
           pieceLabel: {
-            render: 'percentage',
-            fontColor: ['white'],
-            precision: 2
+            render: "percentage",
+            fontColor: ["white"],
+            precision: 2,
           },
 
           tooltips: {
-            enabled: false
+            enabled: false,
           },
 
           scales: {
-            yAxes: [{
-
-              ticks: {
-                display: false
+            yAxes: [
+              {
+                ticks: {
+                  display: false,
+                },
+                gridLines: {
+                  drawBorder: false,
+                  zeroLineColor: "transparent",
+                  color: "rgba(255,255,255,0.05)",
+                },
               },
-              gridLines: {
-                drawBorder: false,
-                zeroLineColor: "transparent",
-                color: 'rgba(255,255,255,0.05)'
-              }
+            ],
 
-            }],
-
-            xAxes: [{
-              barPercentage: 1.6,
-              gridLines: {
-                drawBorder: false,
-                color: 'rgba(255,255,255,0.1)',
-                zeroLineColor: "transparent"
+            xAxes: [
+              {
+                barPercentage: 1.6,
+                gridLines: {
+                  drawBorder: false,
+                  color: "rgba(255,255,255,0.1)",
+                  zeroLineColor: "transparent",
+                },
+                ticks: {
+                  display: false,
+                },
               },
-              ticks: {
-                display: false,
-              }
-            }]
+            ],
           },
-        }
+        },
       });
 
       //CHART HR
       this.canvas = document.getElementById("chartHr");
       this.ctx = this.canvas.getContext("2d");
       this.chartOverheads = new Chart(this.ctx, {
-        type: 'pie',
+        type: "pie",
         data: {
           labels: [1, 2, 3],
-          datasets: [{
-            label: "Cost Breakdown",
-            pointRadius: 0,
-            pointHoverRadius: 0,
-            backgroundColor: [
-              '#49c6ff',
-              '#174d8b'
-            ],
-            borderWidth: 0,
-            data: [management, developer]
-          }]
+          datasets: [
+            {
+              label: "Cost Breakdown",
+              pointRadius: 0,
+              pointHoverRadius: 0,
+              backgroundColor: ["#49c6ff", "#174d8b"],
+              borderWidth: 0,
+              data: [management, developer],
+            },
+          ],
         },
 
         options: {
-
           legend: {
-            display: false
+            display: false,
           },
 
           pieceLabel: {
-            render: 'percentage',
-            fontColor: ['white'],
-            precision: 2
+            render: "percentage",
+            fontColor: ["white"],
+            precision: 2,
           },
 
           tooltips: {
-            enabled: false
+            enabled: false,
           },
 
           scales: {
-            yAxes: [{
-
-              ticks: {
-                display: false
+            yAxes: [
+              {
+                ticks: {
+                  display: false,
+                },
+                gridLines: {
+                  drawBorder: false,
+                  zeroLineColor: "transparent",
+                  color: "rgba(255,255,255,0.05)",
+                },
               },
-              gridLines: {
-                drawBorder: false,
-                zeroLineColor: "transparent",
-                color: 'rgba(255,255,255,0.05)'
-              }
+            ],
 
-            }],
-
-            xAxes: [{
-              barPercentage: 1.6,
-              gridLines: {
-                drawBorder: false,
-                color: 'rgba(255,255,255,0.1)',
-                zeroLineColor: "transparent"
+            xAxes: [
+              {
+                barPercentage: 1.6,
+                gridLines: {
+                  drawBorder: false,
+                  color: "rgba(255,255,255,0.1)",
+                  zeroLineColor: "transparent",
+                },
+                ticks: {
+                  display: false,
+                },
               },
-              ticks: {
-                display: false,
-              }
-            }]
+            ],
           },
-        }
+        },
       });
 
       // CHART PROFITABILITY
       this.canvas = document.getElementById("chartProfitability");
       this.ctx = this.canvas.getContext("2d");
-      this.chartOverheads = new Chart(this.ctx, {
-        type: 'pie',
+      this.chartProfitability = new Chart(this.ctx, {
+        type: "pie",
         data: {
           labels: [1, 2, 3],
-          datasets: [{
-            label: "Profitability",
-            pointRadius: 0,
-            pointHoverRadius: 0,
-            backgroundColor: [
-              '#49c6ff',
-              '#174d8b'
-            ],
-            borderWidth: 0,
-            data: [revenue, cost]
-          }]
+          datasets: [
+            {
+              label: "Profitability",
+              pointRadius: 0,
+              pointHoverRadius: 0,
+              backgroundColor: ["#49c6ff", "#174d8b"],
+              borderWidth: 0,
+              data: [revenue, cost],
+            },
+          ],
         },
 
         options: {
-
           legend: {
-            display: false
+            display: false,
           },
 
           pieceLabel: {
-            render: 'percentage',
-            fontColor: ['white'],
-            precision: 2
+            render: "percentage",
+            fontColor: ["white"],
+            precision: 2,
           },
 
           tooltips: {
-            enabled: false
+            enabled: false,
           },
 
           scales: {
-            yAxes: [{
-
-              ticks: {
-                display: false
+            yAxes: [
+              {
+                ticks: {
+                  display: false,
+                },
+                gridLines: {
+                  drawBorder: false,
+                  zeroLineColor: "transparent",
+                  color: "rgba(255,255,255,0.05)",
+                },
               },
-              gridLines: {
-                drawBorder: false,
-                zeroLineColor: "transparent",
-                color: 'rgba(255,255,255,0.05)'
-              }
+            ],
 
-            }],
-
-            xAxes: [{
-              barPercentage: 1.6,
-              gridLines: {
-                drawBorder: false,
-                color: 'rgba(255,255,255,0.1)',
-                zeroLineColor: "transparent"
+            xAxes: [
+              {
+                barPercentage: 1.6,
+                gridLines: {
+                  drawBorder: false,
+                  color: "rgba(255,255,255,0.1)",
+                  zeroLineColor: "transparent",
+                },
+                ticks: {
+                  display: false,
+                },
               },
-              ticks: {
-                display: false,
-              }
-            }]
+            ],
           },
-        }
+        },
       });
-
     });
-
-
   }
-
 }
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,214 +1,284 @@
-import { Component, OnInit } from '@angular/core';
-import Chart from 'chart.js';
-
+import { HttpErrorResponse } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AdminService } from "app/layouts/admin-layout/admin.service";
+import Chart from "chart.js";
+import { Project } from "../crud/project/model/project";
 
 @Component({
-    selector: 'dashboard-cmp',
-    moduleId: module.id,
-    templateUrl: 'dashboard.component.html'
+  selector: "dashboard-cmp",
+  moduleId: module.id,
+  templateUrl: "dashboard.component.html",
 })
-
-export class DashboardComponent implements OnInit{
-
-  public canvas : any;
+export class DashboardComponent implements OnInit {
+  public projects: Project[] | any;
+  public project: Project | any;
+  public canvas: any;
   public ctx;
-  public chartColor;
-  public chartEmail;
-  public chartHours;
+  public chartOverheads;
+  public chartHr;
+  public chartProfitability;
 
-    ngOnInit(){
+  constructor(private router: Router, private adminService: AdminService) {}
 
-      // LINE CHART
-      this.chartColor = "#FFFFFF";
+  ngOnInit() {
+    this.projects = this.adminService.getProjects();
+    this.adminService
+      .getProjects()
+      .subscribe((projects) => (this.projects = projects));
 
-      this.canvas = document.getElementById("chartHours");
-      this.ctx = this.canvas.getContext("2d");
+    this.adminService.getProjects().subscribe((res) => {
+      let management = 0;
+      let developer = 0;
 
-      this.chartHours = new Chart(this.ctx, {
-        type: 'line',
+      let software = 0;
+      let hardware = 0;
+      let premises = 0;
+      let furniture = 0;
+      let sourcing = 0;
+      let distribution = 0;
 
-        data: {
-          labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
-          datasets: [{
-              borderColor: "#6bd098",
-              backgroundColor: "#6bd098",
-              pointRadius: 0,
-              pointHoverRadius: 0,
-              borderWidth: 3,
-              data: [300, 310, 316, 322, 330, 326, 333, 345, 338, 354]
-            },
-            {
-              borderColor: "#f17e5d",
-              backgroundColor: "#f17e5d",
-              pointRadius: 0,
-              pointHoverRadius: 0,
-              borderWidth: 3,
-              data: [320, 340, 365, 360, 370, 385, 390, 384, 408, 420]
-            },
-            {
-              borderColor: "#fcc468",
-              backgroundColor: "#fcc468",
-              pointRadius: 0,
-              pointHoverRadius: 0,
-              borderWidth: 3,
-              data: [370, 394, 415, 409, 425, 445, 460, 450, 478, 484]
-            }
-          ]
-        },
-        options: {
-          legend: {
-            display: false
-          },
+      let cost = 0;
+      let revenue = 0;
 
-          tooltips: {
-            enabled: false
-          },
+      this.projects.forEach((project) => {
+        management += project.management;
+        developer += project.developer;
 
-          scales: {
-            yAxes: [{
+        software += project.software;
+        hardware += project.hardware;
+        premises += project.premises;
+        furniture += project.furniture;
+        sourcing += project.sourcing;
+        distribution += project.distribution;
 
-              ticks: {
-                fontColor: "#9f9f9f",
-                beginAtZero: false,
-                maxTicksLimit: 5,
-                //padding: 20
-              },
-              gridLines: {
-                drawBorder: false,
-                zeroLineColor: "#ccc",
-                color: 'rgba(255,255,255,0.05)'
-              }
-
-            }],
-
-            xAxes: [{
-              barPercentage: 1.6,
-              gridLines: {
-                drawBorder: false,
-                color: 'rgba(255,255,255,0.1)',
-                zeroLineColor: "transparent",
-                display: false,
-              },
-              ticks: {
-                padding: 20,
-                fontColor: "#9f9f9f"
-              }
-            }]
-          },
-        }
+        cost += project.cost;
+        revenue += project.revenue;
       });
 
-      // PIE CHART
-      this.canvas = document.getElementById("chartEmail");
+      // CHART OVERHEADS
+      this.canvas = document.getElementById("chartOverheads");
       this.ctx = this.canvas.getContext("2d");
-      this.chartEmail = new Chart(this.ctx, {
-        type: 'pie',
+      this.chartOverheads = new Chart(this.ctx, {
+        type: "pie",
         data: {
           labels: [1, 2, 3],
-          datasets: [{
-            label: "Emails",
-            pointRadius: 0,
-            pointHoverRadius: 0,
-            backgroundColor: [
-              '#e3e3e3',
-              '#4acccd',
-              '#fcc468',
-              '#ef8157'
-            ],
-            borderWidth: 0,
-            data: [342, 480, 530, 120]
-          }]
+          datasets: [
+            {
+              label: "Overheads",
+              pointRadius: 0,
+              pointHoverRadius: 0,
+              backgroundColor: [
+                "#49c6ff",
+                "#174d8b",
+                "#c0e6f5",
+                "#952bc7",
+                "#8a76fd",
+                "#961919d0",
+              ],
+              borderWidth: 0,
+              data: [
+                software,
+                hardware,
+                premises,
+                furniture,
+                sourcing,
+                distribution,
+              ],
+            },
+          ],
         },
 
         options: {
-
           legend: {
-            display: false
+            display: false,
           },
 
           pieceLabel: {
-            render: 'percentage',
-            fontColor: ['white'],
-            precision: 2
+            render: "percentage",
+            fontColor: ["white"],
+            precision: 2,
           },
 
           tooltips: {
-            enabled: false
+            enabled: false,
           },
 
           scales: {
-            yAxes: [{
-
-              ticks: {
-                display: false
+            yAxes: [
+              {
+                ticks: {
+                  display: false,
+                },
+                gridLines: {
+                  drawBorder: false,
+                  zeroLineColor: "transparent",
+                  color: "rgba(255,255,255,0.05)",
+                },
               },
-              gridLines: {
-                drawBorder: false,
-                zeroLineColor: "transparent",
-                color: 'rgba(255,255,255,0.05)'
-              }
+            ],
 
-            }],
-
-            xAxes: [{
-              barPercentage: 1.6,
-              gridLines: {
-                drawBorder: false,
-                color: 'rgba(255,255,255,0.1)',
-                zeroLineColor: "transparent"
+            xAxes: [
+              {
+                barPercentage: 1.6,
+                gridLines: {
+                  drawBorder: false,
+                  color: "rgba(255,255,255,0.1)",
+                  zeroLineColor: "transparent",
+                },
+                ticks: {
+                  display: false,
+                },
               },
-              ticks: {
-                display: false,
-              }
-            }]
+            ],
           },
-        }
+        },
       });
 
+      //CHART HR
+      this.canvas = document.getElementById("chartHr");
+      this.ctx = this.canvas.getContext("2d");
+      this.chartHr = new Chart(this.ctx, {
+        type: "pie",
+        data: {
+          labels: [1, 2, 3],
+          datasets: [
+            {
+              label: "Cost Breakdown",
+              pointRadius: 0,
+              pointHoverRadius: 0,
+              backgroundColor: ["#49c6ff", "#174d8b"],
+              borderWidth: 0,
+              data: [management, developer],
+            },
+          ],
+        },
 
-      //DOUBLE LINE CHART
-      var speedCanvas = document.getElementById("speedChart");
+        options: {
+          legend: {
+            display: false,
+          },
 
-      var dataFirst = {
-        data: [0, 19, 15, 20, 30, 40, 40, 50, 25, 30, 50, 70],
-        fill: false,
-        borderColor: '#fbc658',
-        backgroundColor: 'transparent',
-        pointBorderColor: '#fbc658',
-        pointRadius: 4,
-        pointHoverRadius: 4,
-        pointBorderWidth: 8,
-      };
+          pieceLabel: {
+            render: "percentage",
+            fontColor: ["white"],
+            precision: 2,
+          },
 
-      var dataSecond = {
-        data: [0, 5, 10, 12, 20, 27, 30, 34, 42, 45, 55, 63],
-        fill: false,
-        borderColor: '#51CACF',
-        backgroundColor: 'transparent',
-        pointBorderColor: '#51CACF',
-        pointRadius: 4,
-        pointHoverRadius: 4,
-        pointBorderWidth: 8
-      };
+          tooltips: {
+            enabled: false,
+          },
 
-      var speedData = {
-        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-        datasets: [dataFirst, dataSecond]
-      };
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  display: false,
+                },
+                gridLines: {
+                  drawBorder: false,
+                  zeroLineColor: "transparent",
+                  color: "rgba(255,255,255,0.05)",
+                },
+              },
+            ],
 
-      var chartOptions = {
-        legend: {
-          display: false,
-          position: 'top'
-        }
-      };
-
-      var lineChart = new Chart(speedCanvas, {
-        type: 'line',
-        hover: false,
-        data: speedData,
-        options: chartOptions
+            xAxes: [
+              {
+                barPercentage: 1.6,
+                gridLines: {
+                  drawBorder: false,
+                  color: "rgba(255,255,255,0.1)",
+                  zeroLineColor: "transparent",
+                },
+                ticks: {
+                  display: false,
+                },
+              },
+            ],
+          },
+        },
       });
-      
-    }
+
+      // CHART PROFITABILITY
+      this.canvas = document.getElementById("chartProfitability");
+      this.ctx = this.canvas.getContext("2d");
+      this.chartProfitability = new Chart(this.ctx, {
+        type: "pie",
+        data: {
+          labels: [1, 2, 3],
+          datasets: [
+            {
+              label: "Profitability",
+              pointRadius: 0,
+              pointHoverRadius: 0,
+              backgroundColor: ["#49c6ff", "#174d8b"],
+              borderWidth: 0,
+              data: [revenue, cost],
+            },
+          ],
+        },
+
+        options: {
+          legend: {
+            display: false,
+          },
+
+          pieceLabel: {
+            render: "percentage",
+            fontColor: ["white"],
+            precision: 2,
+          },
+
+          tooltips: {
+            enabled: false,
+          },
+
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  display: false,
+                },
+                gridLines: {
+                  drawBorder: false,
+                  zeroLineColor: "transparent",
+                  color: "rgba(255,255,255,0.05)",
+                },
+              },
+            ],
+
+            xAxes: [
+              {
+                barPercentage: 1.6,
+                gridLines: {
+                  drawBorder: false,
+                  color: "rgba(255,255,255,0.1)",
+                  zeroLineColor: "transparent",
+                },
+                ticks: {
+                  display: false,
+                },
+              },
+            ],
+          },
+        },
+      });
+    });
+  }
+
+  goToProject(project: Project) {
+    this.router.navigate(["/project", project.id]);
+  }
+
+  public getProjects(): void {
+    this.adminService.getProjects().subscribe(
+      (response: Project[]) => {
+        this.projects = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
